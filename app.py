b8c0168d4f5aa88c71b4a5fbe694904697eae5e1
mamnuya <69429491+mamnuya@users.noreply.github.com>
2021-06-08 16:08:41 -0400

    Add driver file for portfolio

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/mamnuya")
+def Rinki():
+        return render_template('Rinki.html')
+
+@app.route("/EXAMPLE")
+def Nandhini():
+        return render_template('EXAMPLE.html')
+
+@app.route("/EXAMPLE")
+def Marcela():
+        return render_template('EXAMPLE.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
