8764df8edfc85bd0bc9868d5cfcfc2bbabf56c13
Mamnuya Rinki <69429491+mamnuya@users.noreply.github.com>
2021-06-09 19:07:52 -0400

    Add documentation

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+#links ending in ("/") will redirect to the file within the ()
+#The same style applies to the following methods
+#This allows multiple pages to interact through links.
 @app.route("/")
 def home():
     return render_template('home.html')
 
+#links ending in /mamnuya will redirect to the file within the ()
 @app.route("/mamnuya")
 def Rinki():
-        return render_template('Rinki.html') 
+        return render_template('Rinki.html')
 
 @app.route("/EXAMPLE")
 def Nandhini():
