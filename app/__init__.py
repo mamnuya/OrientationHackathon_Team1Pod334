31e9b4a14bb74c50b7213221c2fe6c59e285a1ce
Mamnuya Rinki <mamnuyarinki@gmail.com>
2021-06-17 16:28:44 -0400

    Restore flask file

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 #This allows multiple pages to interact through links.
 @app.route("/")
 def home():
-    return render_template('app/templates/home.html')
+    return render_template('home.html')
 
 @app.route('/health', methods=['GET'])
 def health():
@@ -19,15 +19,15 @@ def health():
 
 @app.route("/mamnuya")
 def Rinki():
-        return render_template('app/templates/Rinki.html')
+        return render_template('Rinki.html')
 
 @app.route("/nandhini")
 def Nandhini():
-        return render_template('app/templates/Webp.html')
+        return render_template('Webp.html')
 
 @app.route("/ixchel")
 def Marcela():
-        return render_template('app/templates/ixchel.html')
+        return render_template('ixchel.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
