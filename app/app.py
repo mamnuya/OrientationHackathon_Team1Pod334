2f1a4ce7b7e54f342f55bf1d10df1263de9bcac4
Mamnuya Rinki <mamnuyarinki@gmail.com>
2021-06-17 12:03:48 -0400

    Enter file in folder

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+#links ending in ("/") will redirect to the file within the ()
+#The same style applies to the following methods
+#This allows multiple pages to interact through links.
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 200
+
+@app.route("/mamnuya")
+def Rinki():
+        return render_template('Rinki.html')
+
+@app.route("/nandhini")
+def Nandhini():
+        return render_template('Webp.html')
+
+@app.route("/ixchel")
+def Marcela():
+        return render_template('ixchel.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host= '0.0.0.0')
