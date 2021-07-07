f31671c9638ae7d4a21cdfb00319e1fb3ec8a558
Mamnuya Rinki <mamnuyarinki@gmail.com>
2021-07-07 15:10:35 -0400

    Add new line

--- app/db.py
+++ app/db.py
@@ -8,7 +8,8 @@ from flask.cli import with_appcontext
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
-            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
+            current_app.config["DATABASE"],
+            detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
 
